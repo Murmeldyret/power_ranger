0adaa2f4dd04a0793d38bcfb7620ef710974b5e6
Rasmus Hald Nielsen <Murmeldyret@protonmail.com>
2021-11-30 11:42:52 +0100

    Added bellman_ford with comments

--- src/algorithms.c
+++ src/algorithms.c
@@ -1,1 +1,12 @@
 #include "algorithms.h"
+
+/**
+ * Function: bellman_ford
+ * Description: Run Bellman-Ford algorithm
+ * Inputs: graph, source node
+ * Output: distance, predecessor
+ */
+void bellman_ford()
+{
+
+}
