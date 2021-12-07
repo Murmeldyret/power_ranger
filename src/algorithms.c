9451f00d25ba2ea911c5a5344a932299a421bee5
Rasmus Hald Nielsen <Murmeldyret@protonmail.com>
2021-12-07 09:34:56 +0100

    Added comments

--- src/algorithms.c
+++ src/algorithms.c
@@ -38,11 +38,24 @@ void cal_link_weights(igraph_t *graph, struct routerType *routers, struct traffi
     }
 }
 
+
+/**
+ * Function: f
+ * Description: Calculates the weight of each link below 80% utilisation
+ * Inputs: utilisation
+ * Output: weight
+ */
 double f(double x)
 {
     return ((1.0 / 32.0) * pow(x, 2)) - (5.0 * x) + 200.0;
 }
 
+/**
+ * Function: g
+ * Description: Calculates the weight of each link above 80% utilisation
+ * Inputs: utilisation
+ * Output: weight
+ */
 double g(double x)
 {
     return (5.0 / 8.0) * pow(x, 2) - 100.0 * x + 4000.0;
