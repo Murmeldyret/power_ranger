c6e71e01addef948692ddea2fa244063d14508c2
Rasmus Hald Nielsen <Murmeldyret@protonmail.com>
2021-12-13 21:58:08 +0100

    Path is saved to events

--- src/commons.h
+++ src/commons.h
@@ -1,5 +1,18 @@
+#include <igraph/igraph.h>
+
 typedef struct router
 {
     int type;
     double utilisation;
 } router;
+
+typedef struct event
+{
+    int type;
+    int time; //Time of when it happens in seconds.
+    double data; // How much data should be transfered in MB.
+    int source_id;
+    int destination_id;
+    igraph_vector_t path;
+
+} event;
