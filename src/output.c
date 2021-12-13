3e49d9273a5e0c994353ae004f666d6eee1619c2
Mikkel <92383751+Undersnyfler@users.noreply.github.com>
2021-12-13 09:35:31 +0100

    less fucked now, but still need simulation to be done

--- src/output.c
+++ src/output.c
@@ -14,37 +14,29 @@
 void display_data(simulationData *data)
 {
     /* Initialize variables */
-    chain *temp;
-    data->path_ptr = data->current_path;
+    simulationData results;
+    results = *data;
+    results.path_ptr = &results.current_path;
 
     FILE *fp = fopen("outputs/Output.csv", "w");
 
-    if (data != NULL)
-    {
-        printf("initializing data\n");
-        temp = data->path_ptr;
-    }
-    else
-    {
-        printf("Unable to get data\n");
-        return;
-    }
-
     fprintf(fp, "This really be a bruh moment\n");
-    fprintf(fp, "Total power consumption: \n%d Watt(s)\n\n", data->total_power_consumption);
-    fprintf(fp, "Amount of links: \n%d\n\n", data->total_links);
-    fprintf(fp, "Total amount of data: \n%d Mb\n\n", data->total_amount_of_data);
+    fprintf(fp, "Total power consumption: \n%d Watt(s)\n\n", results.total_power_consumption);
+    fprintf(fp, "Amount of links: \n%d\n\n", results.total_links);
+    fprintf(fp, "Total amount of data: \n%d Mb\n\n", results.total_amount_of_data);
 
     fprintf(fp, "Path followed:\n");
-    while (temp)
+
+    // while loop discontinued until function returns actual data, as it is the cause of a segmentation fault
+   /* while (results.path_ptr.n != NULL)
     {
-        fprintf(fp, "%d", temp->data);
-        if (temp->next != NULL)
-        {
-            fprintf(fp, " -> ");
-        }
-        temp = temp->next;
+    }*/
+    fprintf(fp, "%d", results.path_ptr->data);
+    if (results.path_ptr->next != NULL)
+    {
+        fprintf(fp, " -> ");
     }
+    results.path_ptr = results.path_ptr->next;
 
     fprintf(fp, "\n");
 }
