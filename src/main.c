cdfc455ef860c64bc272b33e8467f3c42442f731
Rasmus Hald Nielsen <Murmeldyret@protonmail.com>
2021-12-20 20:20:30 +0100

    Added struct for setup data Added function for recieving data from user

--- src/main.c
+++ src/main.c
@@ -3,6 +3,9 @@
 #include "simulation.h"
 #include "output.h"
 
+
+void recieve_setup(sim_setup *);
+
 /*
  * main - Entry point
  */
@@ -13,7 +16,10 @@ int main(void)
     routerType routertypes[NMBR_OF_ROUTERTYPES];
     trafficType traffictypes[NMBR_OF_TRAFFICTYPES];
     simulationData data;
+    sim_setup setup;
     printf("🗿ඞ\n");
+
+    recieve_setup(&setup);
 
     initialise_data(routertypes, traffictypes);
     //prints structs of relevant type
@@ -25,4 +31,22 @@ int main(void)
     display_data(&data);
 
     return EXIT_SUCCESS; /* Successful termination */
+}
+
+void recieve_setup(sim_setup *setup)
+{
+    printf("-------------------------------------------------------------------------------\n");
+    printf("Enter the number of routers: ");
+    scanf("%d", &setup->nodes);
+    printf("-------------------------------------------------------------------------------\n");
+    printf("Enter the number of links per node: ");
+    scanf("%d", &setup->edges_per_node);
+    printf("-------------------------------------------------------------------------------\n");
+    printf("NOTICE: The simulation will continue to run if there is still data to be sent.\n");
+    printf("Enter the amount of time to simulate in seconds: ");
+    scanf("%d", &setup->simulation_time);
+    printf("-------------------------------------------------------------------------------\n");
+    printf("Enter the number of events to be simulated: ");
+    scanf("%d", &setup->event_count);
+    printf("-------------------------------------------------------------------------------\n");
 }
